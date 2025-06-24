e2b68a0bf2f4ab3c32bd935c59c6d1cc08bdd238
jihenJN <jihenjihen23@gmail.com>
2025-06-24 14:20:18 +0100

    feat :  Create a Modules, Components & Navbar & routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './auth/components/login/login.component';
+import { SignupComponent } from './auth/components/signup/signup.component';
+import { DemoAngularMaterailModule } from './DemoAngularMaterailModule';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    DemoAngularMaterailModule
   ],
   providers: [],
   bootstrap: [AppComponent]
