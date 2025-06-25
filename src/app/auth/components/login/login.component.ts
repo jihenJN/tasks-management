6006f3bfbe5ad7a04e95e8b81cab1e4c1b9cf734
jihenJN <jihenjihen23@gmail.com>
2025-06-25 18:39:13 +0100

    feat: Create Storage Service & Role Base Navigation

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { StorageService } from '../../services/storage/storage.service';
 
 @Component({
   selector: 'app-login',
@@ -33,7 +34,20 @@ loginForm!: FormGroup;
     this.authService.login(this.loginForm.value).subscribe((res)=>{
     console.log(res);
     if(res.userId!=null){
-      this.snackbar.open("Login successful","Close",{duration :5000});
+      const user={
+        id:res.userId,
+        role:res.userRole
+      }
+
+      StorageService.saveUser(user);
+      StorageService.saveToken(res.jwt)
+      if(StorageService.isAdminLoggedIn())
+        this.router.navigateByUrl("/admin/dashboard")
+      else if(StorageService.isEmployeeLoggedIn())
+         this.router.navigateByUrl("/employee/dashboard")
+ this.snackbar.open("Login successful","Close",{duration :5000});
+
+
 
     } else {
       this.snackbar.open("Invalid Credentials","Close",{duration:5000,  panelClass:"error-snackbar"})
